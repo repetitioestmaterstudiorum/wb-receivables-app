8a712205ba0413a4d4610e3bf173a4fb2ef23de0
repetitioestmaterstudiorum <thotan@ik.me>
2020-04-17 15:57:01 +0200

    show invoices on FE

--- imports/api/invoices.js
+++ imports/api/invoices.js
@@ -1,3 +1,10 @@
 import { Mongo } from "meteor/mongo";
 
 export const InvoiceCollection = new Mongo.Collection("invoices");
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish("invoices", function tasksPublication() {
+    return InvoiceCollection.find();
+  });
+}
