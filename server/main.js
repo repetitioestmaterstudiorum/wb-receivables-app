33638eded73548ad263aa06b0b508ae0862147a6
repetitioestmaterstudiorum <thotan@ik.me>
2020-04-19 16:00:05 +0200

    accessing multi form data from sendgrid

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from "meteor/meteor";
+import { WebApp } from "meteor/webapp";
 import bodyParser from "body-parser";
 import { InvoiceCollection } from "/imports/api/invoices";
 import {
@@ -9,19 +10,45 @@ import dotenv from "dotenv";
 dotenv.config({
   path: Assets.absoluteFilePath(".env"),
 });
+const multer = require("multer");
+const multerUpload = multer();
 
 // For requests with content-type JSON:
 WebApp.connectHandlers.use("/email", bodyParser.json());
 // For requests with content-type application/x-www-form-urlencoded
 WebApp.connectHandlers.use("/email", bodyParser.urlencoded({ extended: true }));
+// Use Multer (for multi-form requests) with WebApp but none because there won't be any files
+WebApp.connectHandlers.use("/email", multerUpload.none());
 // Then your handler:
 WebApp.connectHandlers.use("/email", (req, res) => {
-  // API.handleRequest(res, req);
-  console.log(req.body); // for now
+  // logging the request body for test purposes
+  console.log("******************");
+  // console.log("req.body.from", req.body.from);
+  // console.log("req.body.to", req.body.to);
+  console.log("**");
+  const envelope = JSON.parse(req.body.envelope);
+  console.log("envelope.to[0]", envelope.to[0]);
+  console.log("envelope.from", envelope.from);
+  console.log("**");
+  console.log("req.body.subject", req.body.subject);
+  console.log("req.body.email", req.body.email);
+  // multiline string to json object
+  const email = `${req.body.email}`;
+  console.log("************************************************");
+  console.log("************************************************");
+  console.log("email", email);
+  console.log("************************************************");
+
+  // response to sender
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Content-Type", "application/json");
   res.writeHead(200);
-  res.end(JSON.stringify({ status: "ok", content: req.body }));
+  res.end(
+    JSON.stringify({
+      status: "ok",
+      content: req.body,
+    })
+  );
 });
 
 async function fetchAndUpsertInvoices() {
