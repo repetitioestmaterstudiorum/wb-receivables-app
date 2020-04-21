72c1ab4aff3d2a52127b019ed383e7539cd63a50
repetitioestmaterstudiorum <thotan@ik.me>
2020-04-21 10:27:11 +0200

    styling and deleted page

---
+++ imports/ui/pages/Deleted.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Deleted = () => {
+  return (
+    <Container>
+      <div>Deleted</div>
+    </Container>
+  );
+};
+
+export default Deleted;
