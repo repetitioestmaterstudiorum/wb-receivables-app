dd39f7d6dddeffc2240ea320aa7a1faefef5d25d
repetitioestmaterstudiorum <thotan@ik.me>
2020-04-23 17:33:00 +0200

    refactoring

--- imports/ui/pages/Landing.jsx
+++ imports/ui/pages/Landing.jsx
@@ -1,33 +1,15 @@
 import React from "react";
-import { Container, Row, Col, Accordion, Card } from "react-bootstrap";
-import Counter from "../components/Counter";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Landing = () => {
   return (
     <Container>
-      <Row>
-        <Col>
-          <h1>Landing</h1>
-        </Col>
-      </Row>
+      <h1>&#187; Landing</h1>
       <Row>
         <Col>
           <form>Login</form>
         </Col>
       </Row>
-      <br />
-      <Accordion>
-        <Card>
-          <Accordion.Toggle as={Card.Header} eventKey="0">
-            Sample Content
-          </Accordion.Toggle>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>
-              <Counter />
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
     </Container>
   );
 };
