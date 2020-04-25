2ed1cd4f93467ec766d509695c5abdf195a4562e
repetitioestmaterstudiorum <thotan@ik.me>
2020-04-25 21:08:38 +0200

    refactoring and added fetch invoices button

--- imports/api/invoices.js
+++ imports/api/invoices.js
@@ -1,12 +1,20 @@
+import { Meteor } from "meteor/meteor";
 import { Mongo } from "meteor/mongo";
 import { check } from "meteor/check";
 
+import { fetchAndUpsertInvoices } from "./fetchAndUpsertInvoices";
+import { now } from "./now";
+
 export const InvoicesCollection = new Mongo.Collection("invoices");
+export const InvoicesFetchLog = new Mongo.Collection("invoicesfetchlog");
 
 if (Meteor.isServer) {
   // This code only runs on the server
   Meteor.publish("invoices", function () {
     return InvoicesCollection.find();
+  });
+  Meteor.publish("invoicesfetchlog", function () {
+    return InvoicesFetchLog.find();
   });
 }
 
@@ -35,5 +43,17 @@ Meteor.methods({
       { _id: invoiceId },
       { $set: { isPaired: false } }
     );
+  },
+  fetchInvoices() {
+    if (Meteor.isServer) {
+      fetchAndUpsertInvoices();
+    }
+  },
+  updateInvoicesFetchLog() {
+    if (Meteor.isServer) {
+      InvoicesFetchLog.insert({
+        createdAt: now(),
+      });
+    }
   },
 });
--- imports/api/now.js
+++ imports/api/now.js
@@ -1,7 +1,10 @@
+import { Meteor } from "meteor/meteor";
 import moment from "moment-timezone";
 import dotenv from "dotenv";
-dotenv.config({
-  path: Assets.absoluteFilePath(".env"),
-});
+if (Meteor.isServer) {
+  dotenv.config({
+    path: Assets.absoluteFilePath(".env"),
+  });
+}
 
 export const now = () => moment().tz(process.env.TIME_ZONE).format();
